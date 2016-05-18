2eced58deacca5331feb97f5e43e628bee79f08e
Jonathan Stewart <AnorLondo@Jonathans-MacBook-Air.local>
2016-05-18 02:51:15 -0400

    Game now checks for a winner, however is currently broken. Suspect a firebase issue, well fix next commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,6 @@ $( document ).ready( function(){
 $("#console").append('\n' +'<br>');
 } );
 
-
-/*
-$(document).on('click', '.inputButton', function(){
-
-});
-*/
-
 //Function for joining a game
 $("#joinGame").on("click",function(){
 
@@ -53,6 +46,28 @@ $("#joinGame").on("click",function(){
 	return false;
 });
 
+
+$(document).on('click', '.inputButton', function(){
+	if(game.player1LoggedIn && game.player2LoggedIn)
+	{
+		game.playerMove = $(this).data("move");
+		if(game.playerNumber == 1)
+		{		
+			rpsData.child("players").child("1").update({
+				move: game.playerMove
+			});
+		}
+		if(game.playerNumber == 2)
+		{		
+			rpsData.child("players").child("2").update({
+				move: game.playerMove
+			});
+		}
+	}
+
+});
+
+
 $("#submitText").on("click",function(){
 	var textInput = $("#consoleInput").val().trim();
 	$("#consoleInput").val("");
@@ -80,17 +95,20 @@ var game = {
 	player1LoggedIn: false,
 	player2LoggedIn: false,
 	playerName: null,
+	opponentName:null,
 	playerNumber: 0,
-	player1Move: null,
-	player2Move: null,
-	player1MoveChosen: false,
-	player2MoveChosen: false,
-	player1Wins: 0,
-	player2Wins: 0,
-	player1Losses: 0,
-	player2Losses: 0,
+	opponentNumber: 0,
+	playerMove: null,
+	opponentMove: null,
+	playerMoveChosen: false,
+	opponentMoveChosen: false,
+	playerWins: 0,
+	opponentWins: 0,
+	playerLosses: 0,
+	opponentLosses: 0,
 	imageArray:["rock","paper","scissors"],
-	joinGame: function(){
+	joinGame: function()
+	{
 		var textInput = $("#nameInput").val().trim();
 		addTextToConsole(textInput + " has joined the game", true);
 		game.playerName = textInput;
@@ -103,6 +121,82 @@ var game = {
 			$(tempImg).attr("data-move", game.imageArray[i]);
 			$("#selectDiv").append(tempImg);
 		}
+	},
+	determineWinner: function()
+	{
+
+		if (game.playerMove == game.opponentMove ) //covers all ties
+		{
+				game.playersTie();
+		}
+		else if (game.playerMove == 'rock')// user picks rock
+		{
+				if(game.opponentMove== 'scissors')
+				{
+					game.playerWin();
+				}
+				else
+				{
+					game.playerLoss();
+				}
+					
+		}
+		else if(game.playerMove == 'paper') // user picks paper
+		{
+			if(game.opponentMove== 'scissors')
+			{
+					game.playerLoss();
+			}
+			else
+			{
+					game.playerWin();
+			}
+		}
+		else	//user picks scissors
+		{
+			if(game.opponentMove== 'paper')
+			{
+				game.playerWin();
+			}
+			else
+			{
+				game.playerLoss();
+			}
+
+		}
+	},
+	playerWin: function()
+	{
+		game.playerWins ++;
+		rpsData.child("players").child(game.playerNumber).update({
+				wins: game.playerWins
+		});
+		addTextToConsole(game.playerName + " has won the game!", true);
+		addTextToConsole(game.playerName + "- Wins: " +game.playerWins + " Loses: " + game.playerLosses , true);
+		addTextToConsole(game.opponentName + "- Wins: " +game.opponentWins + " Loses: " + game.opponentLosses , true);
+		game.progressGame();
+	},
+	playerLoss: function()
+	{
+		game.playerLosses ++;
+		rpsData.child("players").child(game.playerNumber).update({
+				loses: game.playerLosses
+		});
+		game.progressGame();
+	},
+	playersTie: function()
+	{
+		addTextToConsole("Players have tied the game!", true);
+		addTextToConsole(game.playerName + "- Wins: " +game.playerWins + " Loses: " + game.playerLosses , true);
+		addTextToConsole(game.opponentName + "- Wins: " +game.opponentWins + " Loses: " + game.opponentLosses , true);
+		game.progressGame();
+	},
+	progressGame: function()
+	{
+		game.playerMove = null;
+		game.playerMoveChosen = false;
+		rpsData.child("players").child(game.playerNumber).child("move").remove();
+
 	}
 };
 
@@ -119,6 +213,30 @@ rpsData.on("value", function(snapshot) {
 	game.player1LoggedIn = snapshot.child("players").child("1").exists();
 
 	game.player2LoggedIn = snapshot.child("players").child("2").exists();
+
+
+	//test if both players have logged in
+	if(game.player1LoggedIn && game.player1LoggedIn)
+	{
+		if(game.playerNumber == 1) game.opponentNumber = 2;
+
+		if(game.playerNumber == 2) game.opponentNumber = 1;
+
+		game.opponentName = snapshot.child("players").child(game.opponentNumber).child("name").val();
+		game.opponentWins = snapshot.child("players").child(game.opponentNumber).child("wins").val();
+		game.opponentLosses = snapshot.child("players").child(game.opponentNumber).child("loses").val();
+
+
+		game.playerMoveChosen = snapshot.child("players").child(game.playerNumber).child("move").exists();
+		game.opponentMoveChosen = snapshot.child("players").child(game.opponentNumber).child("move").exists();
+	}
+
+	//if both players have made their move evaluate winner
+	if(game.playerMoveChosen && game.opponentMoveChosen)
+	{
+		game.opponentMove = snapshot.child("players").child(game.opponentNumber).child("move").val();
+		game.determineWinner();
+	}
 
 });
 
